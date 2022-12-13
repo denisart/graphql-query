82be673a074aad47e90722019cd3ba75b2db43ae
Denis Artyushin <artyushinden@gmail.com>
2022-12-13 21:31:57 +0300

    Release/0.0.2 (#2)
    * release: 0.0.2
    
    * release: fix README.md

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'graphql_query'
 copyright = '2022, Denis A. Artyushin'
 author = 'Denis A. Artyushin'
-version = '0.0.1'
+version = '0.0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
