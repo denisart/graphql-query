a73799d7fce97b12a74cf9f6e8a91860f6c578a1
Ilya <grachara@gmail.com>
2023-03-02 17:31:35 +0300

    Fix issue 8 exhaustive fields with no fields kwarg (#9)
    Fix problem https://github.com/denisart/graphql-query/issues/8

---
+++ tests/arguments/test_arguments.py
@@ -0,0 +1,15 @@
+import pytest
+
+from graphql_query import Argument
+
+from .data_arguments import operation_no_fields_kwarg, expected_result_no_fields_kwarg
+
+
+@pytest.mark.parametrize(
+    'operation, result',
+    [
+        (operation_no_fields_kwarg, expected_result_no_fields_kwarg),
+    ]
+)
+def test_valid_query_with_no_fields_kwarg(operation: Argument, result: str):
+    assert operation.render() == result
