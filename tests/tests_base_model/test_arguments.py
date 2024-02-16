225752d329b308ee4b00036ec00495c32fb51018
denisart <artyushinden@gmail.com>
2024-02-16 16:49:27 +0300

    Fix tests
    Add json_schema_extra for PydanticField extra fields

--- tests/tests_base_model/test_arguments.py
+++ tests/tests_base_model/test_arguments.py
@@ -6,7 +6,11 @@ from graphql_query import Argument, Field, GraphQLQueryBaseModel
 def test_fields_with_arguments():
     class Human(GraphQLQueryBaseModel):
         name: str
-        height: float = PydanticField(graphql_arguments=[Argument(name="unit", value="FOOT")])
+        height: float = PydanticField(
+            json_schema_extra={
+                "graphql_arguments": [Argument(name="unit", value="FOOT")],
+            }
+        )
 
     correct = [Field(name="name", fields=[]), Field(name="height", arguments=[Argument(name="unit", value="FOOT")])]
     generated = Human.graphql_fields()
